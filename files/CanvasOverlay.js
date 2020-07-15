920376634d2a76e58ca7ed0cdaca57925acd8097
Michael <clonex_kontakt@hotmail.com>
2020-07-15 10:18:30 +0200

    Optimized cursor && added mouseleave event

--- files/CanvasOverlay.js
+++ files/CanvasOverlay.js
@@ -8,6 +8,7 @@ export default class CanvasOverlay {
     {
         this.bg = createCanvas();
         this.temp = createCanvas();
+        this.cursor = createCanvas();
         this.target = {
             canvas: target,
             ctx: target.getContext("2d")
@@ -15,7 +16,18 @@ export default class CanvasOverlay {
         target.width = window.innerWidth;
         target.height = window.innerHeight;
         this.setup(videoUrl, bgVideo);
+    }
+
+    /*
+     * Loads the given @param videoUrl and sets the video elements source.
+     */
+    async setup(videoUrl, bgVideo)
+    {
         document.addEventListener("mousemove", (e) => this.mouseMoved(e));
+        document.addEventListener("mouseleave", (e) => {
+            this.cursor.canvas.width = this.cursor.canvas.width;
+            this.mousePos = null;
+        });
         document.addEventListener("resize", () => { // TODO: fix me
             target.width = window.innerWidth;
             target.height = window.innerHeight;
@@ -25,16 +37,8 @@ export default class CanvasOverlay {
 
             this.temp.canvas.width = window.innerWidth;
             this.temp.canvas.height = window.innerHeight;
-
-            this.mousePos = null;
         });
-    }
 
-    /*
-     * Loads the given @param videoUrl and sets the video elements source.
-     */
-    async setup(videoUrl, bgVideo)
-    {
         this.background = await createVideo(videoUrl);
         bgVideo.src = this.background.src;
         requestAnimationFrame(() => this.tick());
@@ -50,6 +54,12 @@ export default class CanvasOverlay {
             x: e.pageX,
             y: e.pageY,
         };
+
+        this.cursor.canvas.width = this.cursor.canvas.width;
+        this.cursor.ctx.beginPath();
+        this.cursor.ctx.arc(this.mousePos.x, this.mousePos.y, 145, 0, Math.PI * 2, true);
+        this.cursor.ctx.fillStyle = "red";
+        this.cursor.ctx.fill();
     }
 
     /*
@@ -57,8 +67,7 @@ export default class CanvasOverlay {
      */
     drawCenterText(text)
     {
-        this.temp.canvas.width = this.temp.canvas.width;
-        this.temp.ctx.font = 'bold 54px "Helvetica Neue",-apple-system,BlinkMacSystemFont,Arial,Roboto,Oxygen,Ubuntu,Cantarell,"Open Sans",sans-serif';
+        this.temp.ctx.font = 'bold 4vw "Helvetica Neue",-apple-system,BlinkMacSystemFont,Arial,Roboto,Oxygen,Ubuntu,Cantarell,"Open Sans",sans-serif';
         this.temp.ctx.fillStyle = "red";
         this.metrics = this.temp.ctx.measureText(text);
 
@@ -73,12 +82,12 @@ export default class CanvasOverlay {
     {
         const firstLineI = this.animateI > this.metrics.width/50 ? this.metrics.width/50 : this.animateI;
         this.temp.ctx.fillRect((this.temp.canvas.width / 2) - (this.metrics.width / 2), (this.temp.canvas.height / 2) - (this.metrics.actualBoundingBoxAscent / 2) + 12, 100 + (firstLineI * 50), 5);
-        if(this.animateI >= 50)
+        if(this.animateI >= 10)
         {
-            const tempI = this.animateI >= 100 ? 100 : this.animateI - 50;
-            this.temp.ctx.fillRect((this.temp.canvas.width / 2) - (this.metrics.width / 2), (this.temp.canvas.height / 2) - (this.metrics.actualBoundingBoxAscent / 2) + 18, 100 + (tempI * 30), 100);
+            const tempI = this.animateI >= 60 ? 60 : this.animateI - 10;
+            this.temp.ctx.fillRect((this.temp.canvas.width / 2) - (this.metrics.width / 2), (this.temp.canvas.height / 2) - (this.metrics.actualBoundingBoxAscent / 2) + 17, 100 + (tempI * 30), 100);
             this.temp.ctx.fillRect((this.temp.canvas.width / 2) - (this.metrics.width / 2), (this.temp.canvas.height / 2) - (this.metrics.actualBoundingBoxAscent) - 60, 100 + (tempI * 30), 20);
-            if(this.animateI > 100)
+            if(this.animateI > 60)
             {
                 this.animating = false;
             }
@@ -91,7 +100,6 @@ export default class CanvasOverlay {
      * Runs every iteration.
      */
     async tick(){
-        
         this.drawCenterText("MICHAEL SAABYE SALLING");
         if(this.animating)
         {
@@ -109,6 +117,7 @@ export default class CanvasOverlay {
         var vid = this.bg.ctx.getImageData(0, 0, this.target.canvas.width, this.target.canvas.height).data;
 
         var drawing = this.temp.ctx.getImageData(0, 0, this.target.canvas.width, this.target.canvas.height); // What to overlay the video to
+        var cursor = this.cursor.ctx.getImageData(0, 0, this.target.canvas.width, this.target.canvas.height); 
         for(let i = 0; i < drawing.data.length; i += 4)
         {
             if(drawing.data[i] !== 0 || drawing.data[i + 1] !== 0 || drawing.data[i + 2] !== 0)
@@ -118,26 +127,20 @@ export default class CanvasOverlay {
                 drawing.data[i + 2] = 100 - vid[i + 2];
                 drawing.data[i + 3] = vid[i + 3];
             }
-            
-            if(this.mousePos)
+            if(cursor.data[i] !== 0 || cursor.data[i + 1] !== 0 || cursor.data[i + 2] !== 0)
             {
-                var x = (i / 4) % this.target.canvas.width;
-                var y = ((i / 4) / this.target.canvas.width);
-                if((x - this.mousePos.x) <= 90 && (x - this.mousePos.x) >= -90 && (y - this.mousePos.y) <= 65 && (y - this.mousePos.y) >= -65)
+                if(drawing.data[i] !== 0 || drawing.data[i + 1] !== 0 || drawing.data[i + 2] !== 0)
                 {
-                    if(drawing.data[i] !== 0 || drawing.data[i + 1] !== 0 || drawing.data[i + 2] !== 0)
-                    {
-                        drawing.data[i] = vid[i];
-                        drawing.data[i + 1] = vid[i + 1];
-                        drawing.data[i + 2] = vid[i + 2];
-                        drawing.data[i + 3] = vid[i + 3];
-                    }else{
-                        drawing.data[i] = 255 - vid[i];
-                        drawing.data[i + 1] = 200 - vid[i + 1];
-                        drawing.data[i + 2] = 100 - vid[i + 2];
-                        drawing.data[i + 3] = vid[i + 3];
-                        
-                    }
+                    drawing.data[i] = vid[i];
+                    drawing.data[i + 1] = vid[i + 1];
+                    drawing.data[i + 2] = vid[i + 2];
+                    drawing.data[i + 3] = vid[i + 3];
+                }else{
+                    drawing.data[i] = 255 - vid[i];
+                    drawing.data[i + 1] = 200 - vid[i + 1];
+                    drawing.data[i + 2] = 100 - vid[i + 2];
+                    drawing.data[i + 3] = vid[i + 3];
+                    
                 }
             }
         }

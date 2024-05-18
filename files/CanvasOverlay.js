852555082978f7bc52a2ee9fb1c63b37a9f4e139
Michael Salling <clonex_kontakt@hotmail.com>
2024-05-18 12:30:28 +0200

    Optmized code a bit

--- files/CanvasOverlay.js
+++ files/CanvasOverlay.js
@@ -1,159 +1,163 @@
-import {createCanvas, getUrl, easeIn} from "./helpers.js?2";
+import { createCanvas, getUrl, easeIn } from './helpers.js?2';
 
 export default class CanvasOverlay {
+	constructor(target, videoUrl, bgVideo) {
+		this.animating = true;
+		this.metrics = null;
+		this.mousePos = null;
+		this.animateI = 0;
+		this.boxesData = null;
 
-    constructor(target, videoUrl, bgVideo)
-    {
-        this.animating = true;
-        this.metrics = null;
-        this.mousePos = null;
-        this.animateI = 0;
-        this.boxesData = null;
-        
-        this.bg = createCanvas();
-        this.temp = createCanvas();
-        this.cursor = createCanvas();
-        this.cursorImageData = null;
-        this.target = {
-            canvas: target,
-            ctx: target.getContext("2d")
-        };
-        target.width = window.innerWidth;
-        target.height = window.innerHeight;
-        this.setup(videoUrl, bgVideo);
-    }
+		this.bg = createCanvas();
+		this.temp = createCanvas();
+		this.cursor = createCanvas();
+		this.cursorImageData = null;
+		this.target = {
+			canvas: target,
+			ctx: target.getContext('2d'),
+		};
+		target.width = window.innerWidth;
+		target.height = window.innerHeight;
+		this.setup(videoUrl, bgVideo);
+	}
 
-    /*
-     * Loads the given @param videoUrl and sets the video elements source.
-     */
-    async setup(videoUrl, bgVideo)
-    {
-        document.addEventListener("mousemove", (e) => this.mouseMoved(e));
-        document.addEventListener("mouseleave", (e) => {
-            this.cursorImageData = null;
-        });
+	/*
+	 * Loads the given @param videoUrl and sets the video elements source.
+	 */
+	async setup(videoUrl, bgVideo) {
+		document.addEventListener('mousemove', e => this.mouseMoved(e));
+		document.addEventListener('mouseleave', e => {
+			this.cursorImageData = null;
+		});
 
-        document.addEventListener("resize", () => { // TODO: fix me
-            this.target.width = window.innerWidth;
-            this.target.height = window.innerHeight;
+		document.addEventListener('resize', () => {
+			// TODO: fix me
+			this.target.width = window.innerWidth;
+			this.target.height = window.innerHeight;
 
-            this.bg.canvas.width = window.innerWidth;
-            this.bg.canvas.height = window.innerHeight;
+			this.bg.canvas.width = window.innerWidth;
+			this.bg.canvas.height = window.innerHeight;
 
-            this.temp.canvas.width = window.innerWidth;
-            this.temp.canvas.height = window.innerHeight;
-        });
+			this.temp.canvas.width = window.innerWidth;
+			this.temp.canvas.height = window.innerHeight;
+		});
 
-        this.background = bgVideo;
-        this.background.src = await getUrl(videoUrl);
-        requestAnimationFrame(() => this.tick());
-        document.querySelector(".loading").classList.add("fade-out");
-    }
+		this.background = bgVideo;
+		this.background.src = await getUrl(videoUrl);
+		requestAnimationFrame(() => this.tick());
+		document.querySelector('.loading').classList.add('fade-out');
+	}
 
-    /*
-     * Updates the mouse position.
-     */
-    mouseMoved(e)
-    {  
-        this.mousePos = {
-            x: e.pageX,
-            y: e.pageY,
-        };
+	/*
+	 * Updates the mouse position.
+	 */
+	mouseMoved(e) {
+		this.mousePos = {
+			x: e.pageX,
+			y: e.pageY,
+		};
 
-        this.cursor.canvas.width = this.cursor.canvas.width;
-        this.cursor.ctx.beginPath();
-        this.cursor.ctx.arc(this.mousePos.x, this.mousePos.y, 125, 0, Math.PI * 2, true);
-        this.cursor.ctx.fillStyle = "red";
-        this.cursor.ctx.fill();
-        this.cursorImageData = this.cursor.ctx.getImageData(0, 0, this.target.canvas.width, this.target.canvas.height).data;
-    }
+		this.cursor.canvas.width = this.cursor.canvas.width;
+		this.cursor.ctx.beginPath();
+		this.cursor.ctx.arc(this.mousePos.x, this.mousePos.y, 125, 0, Math.PI * 2, true);
+		this.cursor.ctx.fillStyle = 'red';
+		this.cursor.ctx.fill();
+		this.cursorImageData = this.cursor.ctx.getImageData(0, 0, this.target.canvas.width, this.target.canvas.height).data;
+	}
 
-    /*
-     * Draws @param text in the center of the canvas.
-     */
-    drawCenterText(text)
-    {
-        this.temp.ctx.font = 'bold 4vw "Helvetica Neue",-apple-system,BlinkMacSystemFont,Arial,Roboto,Oxygen,Ubuntu,Cantarell,"Open Sans",sans-serif';
-        this.temp.ctx.fillStyle = "red";
-        this.metrics = this.temp.ctx.measureText(text);
+	/*
+	 * Draws @param text in the center of the canvas.
+	 */
+	drawCenterText(text) {
+		this.temp.ctx.font =
+			'bold 4vw "Helvetica Neue",-apple-system,BlinkMacSystemFont,Arial,Roboto,Oxygen,Ubuntu,Cantarell,"Open Sans",sans-serif';
+		this.temp.ctx.fillStyle = 'red';
+		this.metrics = this.temp.ctx.measureText(text);
 
-        this.temp.ctx.fillText(text, (this.temp.canvas.width / 2) - (this.metrics.width / 2), (this.temp.canvas.height / 2) - (this.metrics.actualBoundingBoxAscent / 2));
-    }
+		this.temp.ctx.fillText(
+			text,
+			this.temp.canvas.width / 2 - this.metrics.width / 2,
+			this.temp.canvas.height / 2 - this.metrics.actualBoundingBoxAscent / 2
+		);
+	}
 
-    /*
-     * Animates the boxes.
-     */
-    animateBoxes()
-    {
-        const firstLineI = this.animateI > this.metrics.width / 45 ? this.metrics.width / 45 : this.animateI;
-        this.temp.ctx.fillRect((this.temp.canvas.width / 2) - (this.metrics.width / 2), (this.temp.canvas.height / 2) - (this.metrics.actualBoundingBoxAscent / 2) + 12, 100 + easeIn(firstLineI), 5);//(firstLineI * 50), 5);
-        if(this.animateI >= 10)
-        {
-            const tempI = this.animateI >= 60 ? 60 : this.animateI - 10;
-            this.temp.ctx.fillRect((this.temp.canvas.width / 2) - (this.metrics.width / 2), (this.temp.canvas.height / 2) - (this.metrics.actualBoundingBoxAscent / 2) + 17, 100 + easeIn(tempI), 100);
-            this.temp.ctx.fillRect((this.temp.canvas.width / 2) - (this.metrics.width / 2), (this.temp.canvas.height / 2) - (this.metrics.actualBoundingBoxAscent) - 60, 100 + easeIn(tempI), 20);
-            if(this.animateI > 60)
-            {
-                this.animating = false;
-            }
-        }
-        this.animateI++;
-    }
-    
-    /*
-     * Redraws the canvases and updates the animations every frame.
-     */
-    async tick(){
-        if(this.animating)
-        {
-            this.drawCenterText("MICHAEL SAABYE SALLING");
-            this.animateBoxes();
-        }
-        this.draw();
-        requestAnimationFrame(() => this.tick());
-    }
+	/*
+	 * Animates the boxes.
+	 */
+	animateBoxes() {
+		const firstLineI = this.animateI > this.metrics.width / 45 ? this.metrics.width / 45 : this.animateI;
+		this.temp.ctx.fillRect(
+			this.temp.canvas.width / 2 - this.metrics.width / 2,
+			this.temp.canvas.height / 2 - this.metrics.actualBoundingBoxAscent / 2 + 12,
+			100 + easeIn(firstLineI),
+			5
+		); //(firstLineI * 50), 5);
+		if (this.animateI >= 10) {
+			const tempI = this.animateI >= 60 ? 60 : this.animateI - 10;
+			this.temp.ctx.fillRect(
+				this.temp.canvas.width / 2 - this.metrics.width / 2,
+				this.temp.canvas.height / 2 - this.metrics.actualBoundingBoxAscent / 2 + 17,
+				100 + easeIn(tempI),
+				100
+			);
+			this.temp.ctx.fillRect(
+				this.temp.canvas.width / 2 - this.metrics.width / 2,
+				this.temp.canvas.height / 2 - this.metrics.actualBoundingBoxAscent - 60,
+				100 + easeIn(tempI),
+				20
+			);
+			if (this.animateI > 60) {
+				this.animating = false;
+			}
+		}
+		this.animateI++;
+	}
 
-    /*
-     * Draws the canvases, and converts the data to the target canvas.
-     */
-    draw(){
-        this.bg.ctx.drawImage(this.background, 0, 0, this.target.canvas.width, this.target.canvas.height); // Draw video
-        let vid = this.bg.ctx.getImageData(0, 0, this.target.canvas.width, this.target.canvas.height).data;
+	/*
+	 * Redraws the canvases and updates the animations every frame.
+	 */
+	tick() {
+		if (this.animating) {
+			this.drawCenterText('MICHAEL SAABYE SALLING');
+			this.animateBoxes();
+		}
+		this.draw();
+		requestAnimationFrame(() => this.tick());
+	}
 
-        if(this.animating)
-        {
-            this.boxesData = this.temp.ctx.getImageData(0, 0, this.target.canvas.width, this.target.canvas.height).data; // What to overlay the video to
-        }
+	/*
+	 * Draws the canvases, and converts the data to the target canvas.
+	 */
+	draw() {
+		this.bg.ctx.drawImage(this.background, 0, 0, this.target.canvas.width, this.target.canvas.height); // Draw video
+		let vid = this.bg.ctx.getImageData(0, 0, this.target.canvas.width, this.target.canvas.height).data;
 
-        let drawing = new Uint8ClampedArray(this.boxesData);
-        for(let i = 0; i < drawing.byteLength; i += 4)
-        {
-            if(drawing[i] !== 0 || drawing[i + 1] !== 0 || drawing[i + 2] !== 0)
-            {
-                drawing[i] = 255 - vid[i];
-                drawing[i + 1] = 200 - vid[i + 1];
-                drawing[i + 2] = 100 - vid[i + 2];
-                drawing[i + 3] = vid[i + 3];
-            }
+		if (this.animating) {
+			this.boxesData = this.temp.ctx.getImageData(0, 0, this.target.canvas.width, this.target.canvas.height).data; // What to overlay the video to
+		}
 
-            if(this.cursorImageData && (this.cursorImageData[i] !== 0 || this.cursorImageData[i + 1] !== 0 || this.cursorImageData[i + 2] !== 0))
-            {
-                if(drawing[i] !== 0 || drawing[i + 1] !== 0 || drawing[i + 2] !== 0)
-                {
-                    drawing[i] = vid[i];
-                    drawing[i + 1] = vid[i + 1];
-                    drawing[i + 2] = vid[i + 2];
-                    drawing[i + 3] = vid[i + 3];
-                }else{
-                    drawing[i] = 255 - vid[i];
-                    drawing[i + 1] = 200 - vid[i + 1];
-                    drawing[i + 2] = 100 - vid[i + 2];
-                    drawing[i + 3] = vid[i + 3];
-                }
-            }
-        }
+		let drawing = new ImageData(
+			new Uint8ClampedArray(this.boxesData),
+			this.target.canvas.width,
+			this.target.canvas.height
+		);
+		for (let i = 0; i < drawing.data.byteLength; i += 4) {
+			if (drawing.data[i] !== 0 || drawing.data[i + 1] !== 0 || drawing.data[i + 2] !== 0) {
+				drawing.data.set([255 - vid[i], 200 - vid[i + 1], 100 - vid[i + 2], vid[i + 3]], i);
+			}
 
-        const output = new ImageData(drawing, this.target.canvas.width, this.target.canvas.height);
-        this.target.ctx.putImageData(output, 0, 0);
-    }
+			if (
+				this.cursorImageData &&
+				(this.cursorImageData[i] !== 0 || this.cursorImageData[i + 1] !== 0 || this.cursorImageData[i + 2] !== 0)
+			) {
+				if (drawing.data[i] !== 0 || drawing.data[i + 1] !== 0 || drawing.data[i + 2] !== 0) {
+					drawing.data.set([vid[i], vid[i + 1], vid[i + 2], vid[i + 3]], i);
+				} else {
+					drawing.data.set([255 - vid[i], 200 - vid[i + 1], 100 - vid[i + 2], vid[i + 3]], i);
+				}
+			}
+		}
+
+		this.target.ctx.putImageData(drawing, 0, 0);
+	}
 }
--- files/helpers.js
+++ files/helpers.js
@@ -1,44 +1,44 @@
 /*
  * Returns a video element, with the source of @param url.
  */
-export const createVideo = async (url) => {
-    const data = await getUrl(url);
-    
-    const video = document.createElement("video");
+export const createVideo = async url => {
+	const data = await getUrl(url);
 
-    video.autoplay = true;
-    video.loop = true;
-    video.muted = true;
-    video.playsinline = true;
-    video.src = window.URL.createObjectURL(data);
-    video.play();
-    return video;
+	const video = document.createElement('video');
+
+	video.autoplay = true;
+	video.loop = true;
+	video.muted = true;
+	video.playsinline = true;
+	video.src = window.URL.createObjectURL(data);
+	video.play();
+	return video;
 };
 
 /*
  * Easings.
  */
-export const easeIn = (x)  => (x * x * x) / (x / 2);
+export const easeIn = x => (x * x * x) / (x / 2);
 
 /*
  * Gets the content of @param url, and returns a local blob URL.
  */
-export const getUrl = async (url) => {
-    const data = await fetch(url).then(d => d.blob()); // TODO: catch error
-    return window.URL.createObjectURL(data);
+export const getUrl = async url => {
+	const data = await fetch(url).then(d => d.blob()); // TODO: catch error
+	return window.URL.createObjectURL(data);
 };
 
 /*
- * Returns a fresh canvas and context. 
+ * Returns a fresh canvas and context.
  */
 export const createCanvas = () => {
-    const canvas = document.createElement("canvas");
-    const ctx = canvas.getContext("2d");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+	const canvas = document.createElement('canvas');
+	const ctx = canvas.getContext('2d');
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
 
-    return {
-        canvas,
-        ctx
-    };
+	return {
+		canvas,
+		ctx,
+	};
 };

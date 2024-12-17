6b43ca796c8b596fee46b1eccbb38ccac9249a48
Michael Salling <clonex_kontakt@hotmail.com>
2024-12-17 17:43:47 +0100

    Added logging

--- Backend/src/crons/crawlGomore.ts
+++ Backend/src/crons/crawlGomore.ts
@@ -35,6 +35,8 @@ export async function crawlGomoreSellers() {
       value: rental.amount.toString(),
     })),
   });
+
+  console.log("Crawled rentals", sellerRentals);
 }
 
 interface GomoreSection {
